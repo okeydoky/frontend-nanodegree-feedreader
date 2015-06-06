71fa04d74f2f6641c36a44ae0d1ef6515af0fd02
Ching-Hung Lin <okeydoky@gmail.com>
2015-06-06 18:23:53 -0400

    Add spec: menu show/hide when menu-icon is clicked

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -54,19 +54,32 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('Menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+        it('is hidden by default', function() {
+           expect($('body').hasClass('menu-hidden')).toBeTruthy(); 
+        });
+        
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+        it('will appear/hide when menu icon is clicked', function() {
+            var $menuIcon = $('.menu-icon-link');
+            // click the first time; menu should appear
+            $menuIcon.trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBeFalsy();
+            
+            // click the second time; menu should disappear
+            $menuIcon.trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBeTruthy();
+        });
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
